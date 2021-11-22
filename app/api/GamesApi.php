479d9e7abe8070a44b75546f1a1bce80ae1089be
RaphaelStn <raphaelstacino@gmail.com>
2021-11-22 11:52:55 +0100

    update curl method

--- app/api/GamesApi.php
+++ app/api/GamesApi.php
@@ -5,6 +5,7 @@ use \Core\Api\Api;
 class GamesApi extends Api {
     public function getTrendingGames() {
         $this->data = $this->getCurl('https://api.rawg.io/api/games?key=420c1efcd72a4af893d68e8e8378211d&dates=2019-09-01,2019-09-30&platforms=18,1,7');
-        return array_slice($this->data, 0, 3);
+        shuffle($this->data);
+        return array_slice($this->data, 0, 4);
     }
 }
--- app/api/MoviesApi.php
+++ app/api/MoviesApi.php
@@ -6,10 +6,12 @@ class MoviesApi extends Api {
 
     public function getTrendingMovies() {
         $this->data = $this->getCurl('https://api.themoviedb.org/3/trending/movie/week?api_key=ab958811b56c320d1a3731b7d8afe657');
-        return array_slice($this->data,0, 3);
+        shuffle($this->data);
+        return array_slice($this->data, 0, 4);
     }
     public function getTrendingShows() {
         $this->data = $this ->getCurl('https://api.themoviedb.org/3/trending/tv/week?api_key=ab958811b56c320d1a3731b7d8afe657');
-        return array_slice($this->data, 0, 3);
+        shuffle($this->data);
+        return array_slice($this->data, 0, 4);
     }
 }
