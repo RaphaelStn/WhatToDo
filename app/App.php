d0f36bec9cdf1bb326d36caa6212fc8d1496dbeb
RaphaelStn <raphaelstacino@gmail.com>
2021-12-28 11:08:14 +0100

    ajout d'intentation

--- app/App.php
+++ app/App.php
@@ -3,7 +3,7 @@ use Core\Config;
 class App {
     public static $_instance;
     
-    public static function load(){ // load les Autoloader et le sessionstart().
+    public static function load(){ 
         session_start();
         require ROOT . '/app/AutoLoader.php';
         App\Autoloader::register();
@@ -12,15 +12,15 @@ class App {
         // Loading twig
         require ROOT . '/vendor/autoload.php';
     }    
-    public static function getInstance() {
+    public static function getInstance() { // use of a singleton for loading the same instance of App
         if(self::$_instance === null){ 
             self::$_instance = new App();
         }
         return self::$_instance;
     }
-    public function getApi($name) { 
-        $this -> config = Config::getInstance(ROOT . '/config/config.php');
-        $class_name = '\\App\\Api\\' . ucfirst($name) . 'Api';
+    public function getApi($name) {  // Launch API's classes with config keys in constructor
+        $this -> config = Config::getInstance(ROOT . '/config/config.php'); // Return API keys
+        $class_name = '\\App\\Api\\' . ucfirst($name) . 'Api'; 
         return new $class_name($this -> config -> get('key_movie'), $this -> config -> get('key_game'));
     }
 }
